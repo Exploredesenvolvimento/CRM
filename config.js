680a3f1f5ae948b32383fe379622bf0decc86343
Exploredesenvolvimento <jduquebpm@gmail.com>
2026-05-19 16:19:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Substitua pelos valores do seu projeto Supabase
 // ═══════════════════════════════════════════════
 const SUPABASE_URL = 'https://jczhnuhvgobobbyuucjr.supabase.co/rest/v1/';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjemhudWh2Z29ib2JieXV1Y2pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMDc3MTcsImV4cCI6MjA5NDc4MzcxN30.00zevN6QODnQcJhmRRVMxJgrOBnk2LG3PoN8p_IQ7ek';
+const SUPABASE_ANON_KEY = 'sb_publishable_osljOy4Nwu9E7qbglhc3kA_2x-fYhDd';
